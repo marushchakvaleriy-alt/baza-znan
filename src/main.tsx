3efe4616d20a0efb31bdf8951d0dfa3166a48299
Марущак Валерій <v_marushchak@viyar.com.ua>
2026-03-06 14:23:57 +0200

    Fix router basename for gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </AuthProvider>
